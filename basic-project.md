80732208e7578dc2769abd39f825881a29681d1b
pdn <pnathan@vandals.uidaho.edu>
2013-01-09 23:22:04 -0800

    Fixing the build script.
    Renamed the index.md so that it'd be a index.html

--- basic-project.md
+++ basic-project.md
@@ -1,20 +1,21 @@
-Basic Project
-===
+% Basic Project
+%
+%
 
 We are going to assume that your first project is a small standalone
 program using one of the common libraries for Common Lisp. Since
 Common Lisp is a general purpose high-level programming langugage, it
 can handle any common high-level language task.
 
-Common libraries are installed using Quicklisp.
+Common libraries are installed using [Quicklisp][Quicklisp]!
 
-http://www.quicklisp.org/ Go over there and install it!
+Go over there and install it, then come back to this tutorial.
 
 
 Let's assume for the purpose of the starting project that you want to
 recursively walk a web server and take some action based upon the
 internal contents. We've supplied a minimal (not very robust or
-general) solution in "web-trotter.lisp", available from <url>.
+general) solution in `web-trotter.lisp`, available from [our site](src/web-trotter.lisp).
 
 Without examining the details of the Trotter, notice that there's a
 line:
@@ -34,3 +35,5 @@ tool that would feed in the data to a database for searching.
 
 Note that there is a lack of a main function. Lisp has no specified
 MAIN, unlike Java or C.
+
+[Quicklisp]: http://www.quicklisp.org/
--- make.pl
+++ make.pl
@@ -1,10 +1,54 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
 use 5.010;
 use File::Basename;
 
-foreach my $FN (split(/\n/, `ls *.md`)) {
-    $FN =~ s/^(.+)\.md/$1/g;
-    say $FN;
-    `pandoc --ascii --smart --normalize --highlight-style=haddock -f markdown -t html5 $FN.md -o output/$FN.html`;
+sub build_banner {
+    my ($active, @members) = @_;
+    my $html='
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">articulate-lisp.com</a>
+          <div class="nav-collapse collapse">
+    <ul class="nav">
+<!--    <li class="active">
+    </li> -->
+    ';
+    foreach my $fn (@members) {
+        my $active_str = '';
+        if ($fn eq $active) {
+            $active_str = ' class="active" ';
+        }
+        $html .= "<li $active_str><a href=\"$fn.html\">" . $fn . "</a></li>\n";
+    }
+    $html .= '</ul>
+</div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>';
+
+    return $html;
 }
+
+
+my @FNs = map { $_ =~ s/^(.+)\.md/$1/g; $_} split(/\n/, `ls *.md`);
+my $fh;
+
+foreach my $FN (@FNs) {
+    open( $fh, ">", "banner.html");
+    print $fh build_banner($FN, @FNs);
+    close $fh;
+    my $cmd ="pandoc -B banner.html -T 'Articulate Common Lisp' --template=pandoc-data/templates/default.html5 --ascii --smart --normalize --highlight-style=haddock -f markdown -t html5 $FN.md -o site/$FN.html";
+
+    say $cmd;
+    `$cmd`;
+}
+
+`cp -r src site/`;
