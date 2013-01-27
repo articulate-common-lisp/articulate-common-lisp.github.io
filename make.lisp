7fe5fa71a308c9b0e5466689020ac72ff88a1145
pnathan <pnathan@vandals.uidaho.edu>
2013-01-27 00:47:52 -0800

    Point the big top-bar title to index.html

--- make.lisp
+++ make.lisp
@@ -37,7 +37,7 @@
             <span class=\"icon-bar\"></span>
             <span class=\"icon-bar\"></span>
           </a>
-          <a class=\"brand\" href=\"#\">articulate-lisp.com</a>
+          <a class=\"brand\" href=\"index.html\">articulate-lisp.com</a>
           <div class=\"nav-collapse collapse\">
             <ul class=\"nav\">")
         (final-html
