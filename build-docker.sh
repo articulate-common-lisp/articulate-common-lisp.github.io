fb3768a93650026abd4e2631fc725c1bf539633e
Paul Nathan <pnathan@alumni.uidaho.edu>
2015-07-31 21:58:36 -0700

    Moving to the proper directory

--- build-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-make
-docker build -t pnathan/articulate-common-lisp:latest .
--- jekyll/examples/src/sausage.lisp
+++
@@ -1,9 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; sausage.lisp
-;;;;
-;;;; Library of routiens for wakling the web.
-;;;;
-;;;; Author: Paul Nathan
-;;;; License: AGPL3
-
-;; TODO. A cleanup of trotter.lisp
